4e64c7554089f0e585c3b9ebedfceee596726d25
amro22397 <amroalmutasim22@gmail.com>
2025-01-05 15:59:19 +0400

    hello

--- app/page/page.tsx
+++ app/page/page.tsx
@@ -4,11 +4,11 @@ import { useSession } from 'next-auth/react'
 import React from 'react'
 
 const page = async () => {
-    const session = useSession();
+    
 
   return (
     <div>
-        {session.data?.user?.email}
+        Hello
     </div>
   )
 }
