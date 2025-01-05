bec04570f674e6fb4b069e3ce3cf986f25a59def
amro22397 <amroalmutasim22@gmail.com>
2025-01-05 06:32:27 +0400

    update maximum duration

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ const page = () => {
     <div className='flex justify-center items-center h-screen poppins'>
       <LoginPage />
       <div className=""></div>
+      <div className=""></div>
     </div>
   )
 }
