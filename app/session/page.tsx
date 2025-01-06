cea711eee2744116152c696745d85d237f4f9e06
amro22397 <amroalmutasim22@gmail.com>
2025-01-06 15:42:01 +0400

    try session

--- app/session/page.tsx
+++ app/session/page.tsx
@@ -1,9 +1,12 @@
+import { useSession } from 'next-auth/react';
 import React from 'react'
 
 const page = () => {
+    const session = useSession();
   return (
     <div>
       Hello
+      {session.data?.user?.email}
     </div>
   )
 }
