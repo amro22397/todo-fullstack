fd6a38e18f77cc54327370dbed58c97e44ea43da
amro22397 <amroalmutasim22@gmail.com>
2025-01-06 07:48:29 +0400

    sdsadas

--- app/page/page.tsx
+++ app/page/page.tsx
@@ -13,17 +13,14 @@ const page = async () => {
   const session = await getUser();
     console.log(session);
 
-    mongoose.connect(process.env.MONGO_URL as string)
-        const tasks = await Tasks.find({userEmail: {$in: [session?._doc?.email]}}, {}, {sort: {createdAt: -1}});
+   // mongoose.connect(process.env.MONGO_URL as string)
+    // const tasks = await Tasks.find({userEmail: {$in: [session?._doc?.email]}}, {}, {sort: {createdAt: -1}});
   return (
     <div>
         Hello
 
-        <div>{JSON.stringify(session, null, 2)}</div>
-        <div className=""></div>
-        <div>{JSON.stringify(tasks, null, 2)}</div>
-        <div className=""></div>
-        <div className=""></div>
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+        
         
     </div>
   )
