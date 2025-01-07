ee3cdc29998377a06f685ee731710f2d4a53404c
amro22397 <amroalmutasim22@gmail.com>
2025-01-07 07:47:50 +0400

    dbConn()

--- server/controllers/tasksContoller.js
+++ server/controllers/tasksContoller.js
@@ -1,8 +1,11 @@
 import { Tasks } from "../../models/tasks"
+import { dbConn } from "../dbConn/dbConn"
 
 
 export const getTasks = async () => {
     try {
+        await dbConn();
+        
         const getTasks = await Tasks.find();
         const tasks = JSON.parse(JSON.stringify(getTasks));
         if (tasks) return tasks;
